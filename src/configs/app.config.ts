bfdb79895144d25d0df2ab7dda31f24ee9b1b79b
ginomassei <ginomassei@icloud.com>
2023-05-15 15:50:17 -0300

    chore(docker): docker-compose and more configs

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -3,10 +3,13 @@ export const config = {
     port: process.env.PORT || 3000
   },
   cors: {
-    origins: ['http://localhost:4200', 'http://localhost:9000']
+    origins: ['http://localhost:4200', 'http://localhost:9000', 'http://datamarket-front:80']
   },
   database: {
-    dbHost: process.env.DB_URI || '127.0.0.1:27017'
+    dbHost: process.env.DB_URI || '127.0.0.1:27017',
+    dbName: process.env.DB_NAME || 'test',
+    dbUser: process.env.DB_USER || '',
+    dbPassword: process.env.DB_PASSWORD || ''
   },
   isDevEnvironment: process.env.NODE_ENV === 'development'
 };
--- src/configs/database.config.ts
+++ src/configs/database.config.ts
@@ -7,6 +7,15 @@ import { config } from './app.config';
 export class DatabaseConfig {
   static async connectDatabase(logger: ILogger) {
     providerLogger.setLogger(logger);
-    await ManagerDb.connect(false, 'test', config.database.dbHost, '', false, Provider.None);
+    await ManagerDb.connect(
+      false,
+      config.database.dbName,
+      config.database.dbHost,
+      '',
+      config.isDevEnvironment,
+      Provider.None,
+      config.database.dbUser,
+      config.database.dbPassword
+    );
   }
 }
